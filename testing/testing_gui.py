eac74e1aadf62a6f4972a62d15483029a0c93937
Michael L. Cox <m1cox@thig.com>
2018-12-17 09:48:20 -0500

    Added some testing

---
+++ testing/testing_gui.py
@@ -0,0 +1,5 @@
+# TODO: Write all these tests
+# Superpower button is present
+# /sr/ & starred & subscribbed & schedules list definitions
+# Clicking on a definition it should show the description, list reports with the file name, date, and downloads.
+# Clicking on a report downloads count should show usernames and timestamps
