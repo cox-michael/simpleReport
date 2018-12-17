42eef85c53f324e52a03f60904cfa1c28fb16c20
Michael L. Cox <m1cox@thig.com>
2018-12-17 09:55:21 -0500

    Added three measures to the analytics page

--- src/analytics.js
+++ src/analytics.js
@@ -4,6 +4,9 @@ import { withStyles } from '@material-ui/core/styles';
 import { SessionContext } from "./session";
 import Grid from '@material-ui/core/Grid';
 import MostDownloadedReports from './analytics/most_downloaded_reports';
+import MostDownloadedDefinitions from './analytics/most_downloaded_definitions';
+import MostSubscribedToDefinitions from './analytics/most_subscribed_to_definitions';
+import MostStarredDefinitions from './analytics/most_starred_definitions';
 import MostActiveUsersByDownloads from './analytics/most_active_users_by_downloads';
 import ReportsGeneratedByDate from './analytics/reports_generated_by_date';
 import Paper from '@material-ui/core/Paper';
@@ -37,7 +40,10 @@ class Analytics extends React.Component {
         alignItems="flex-start"
       >
         <Paper className={classes.paper}><MostDownloadedReports /></Paper>
+        <Paper className={classes.paper}><MostDownloadedDefinitions /></Paper>
         <Paper className={classes.paper}><MostActiveUsersByDownloads /></Paper>
+        <Paper className={classes.paper}><MostSubscribedToDefinitions /></Paper>
+        <Paper className={classes.paper}><MostStarredDefinitions /></Paper>
         <Paper className={classes.paper}><ReportsGeneratedByDate /></Paper>
       </Grid>
     );
