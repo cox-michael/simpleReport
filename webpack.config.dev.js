276ece210bc95fb78b750554e05f281874a307e1
Michael L. Cox <m1cox@sflgnvana03.thig.com>
2019-07-10 17:23:39 -0400

    Updated config files and modules

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,17 +1,29 @@
 const dotenv = require('dotenv');
 const webpack = require('webpack');
+const path = require('path');
 // const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = () => {
   const env = dotenv.config().parsed;
 
   const envKeys = Object.keys(env).reduce((prev, next) => {
+    // eslint-disable-next-line no-param-reassign
     prev[`process.env.${next}`] = JSON.stringify(env[next]);
     return prev;
   }, {});
 
   return {
     entry: './src/index.js',
+    output: {
+      // `filename` provides a template for naming your bundles (remember to use `[name]`)
+      filename: '[name].js',
+      // `chunkFilename` provides a template for naming code-split bundles (optional)
+      chunkFilename: '[name].js',
+      // `path` is the folder where Webpack will place your bundles
+      path: path.resolve(__dirname, 'dist'),
+      // `publicPath` is where Webpack will load your bundles from (optional)
+      publicPath: 'dist/',
+    },
     mode: 'development',
     watch: true,
     watchOptions: {
@@ -26,17 +38,22 @@ module.exports = () => {
           options: {
             cacheDirectory: true,
           },
-        }, ],
-      }, ],
+        }],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader', 'eslint-loader'],
+      }],
     },
     plugins: [
-      new webpack.DefinePlugin(envKeys)
+      new webpack.DefinePlugin(envKeys),
       // new HtmlWebpackPlugin({
-      // 	API_URL: process.env.API_URL,
+      //  API_URL: process.env.API_URL,
       //   filename: 'index.html',
       //   template: 'src/index_template.html',
-      // 	inject: false
+      //  inject: false
       // })
-    ]
+    ],
   };
 };
--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,29 @@
 const dotenv = require('dotenv');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
 
 module.exports = () => {
   const env = dotenv.config().parsed;
 
   const envKeys = Object.keys(env).reduce((prev, next) => {
+    // eslint-disable-next-line no-param-reassign
     prev[`process.env.${next}`] = JSON.stringify(env[next]);
     return prev;
   }, {});
 
   return {
     entry: './src/index.js',
+    output: {
+      // `filename` provides a template for naming your bundles (remember to use `[name]`)
+      filename: '[name].js',
+      // `chunkFilename` provides a template for naming code-split bundles (optional)
+      chunkFilename: '[name].js',
+      // `path` is the folder where Webpack will place your bundles
+      path: path.resolve(__dirname, 'dist'),
+      // `publicPath` is where Webpack will load your bundles from (optional)
+      publicPath: 'dist/',
+    },
     mode: 'production',
     module: {
       rules: [{
@@ -22,8 +34,8 @@ module.exports = () => {
           options: {
             cacheDirectory: true,
           },
-        }, ],
-      }, ],
+        }],
+      }],
     },
     plugins: [
       new webpack.DefinePlugin(envKeys),
@@ -31,8 +43,8 @@ module.exports = () => {
         API_URL: process.env.API_URL,
         filename: 'index.html',
         template: 'src/index_template.html',
-        inject: false
-      })
-    ]
+        inject: false,
+      }),
+    ],
   };
 };
