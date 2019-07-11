944347f8c4d3a36487962ec42b9a59f4b800cf23
Michael L. Cox <m1cox@sflgnvana03.thig.com>
2019-07-11 08:47:31 -0400

    Simplifying

--- src/index.js
+++ src/index.js
@@ -1,22 +1,9 @@
-console.log('Environment: ' + process.env.NODE_ENV);
-console.log('API_URL: ' + process.env.API_URL);
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
 
-import React, { Component } from 'react'
-import ReactDOM from 'react-dom'
-import App from './app'
+if (process.env.NODE_ENV !== 'production') console.log(`Environment: ${process.env.NODE_ENV}`);
+// console.log(`API_URL: ${process.env.API_URL}`);
 
-// these 7 lines are just to remove the deprecation warning
-import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
-const theme = createMuiTheme({
-  typography: {
-    useNextVariants: true,
-    suppressDeprecationWarnings: true,
-  },
-});
-// delete the MuiThemeProvider wrapper below when deprecation completes
-const root = document.querySelector('#root')
-ReactDOM.render(
-  <MuiThemeProvider theme={theme}>
-  <App />
-  </MuiThemeProvider>
-  , root)
+const root = document.querySelector('#root'); // eslint-disable-line no-undef
+ReactDOM.render(<App />, root);
