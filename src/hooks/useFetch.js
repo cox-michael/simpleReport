97a4a702e724e433125be67f77f2f2ee663b3c05
Michael L. Cox <m1cox@sflgnvana03.thig.com>
2019-08-05 09:35:31 -0400

    promisifying useFetch and adding GET fetch method

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,38 +1,57 @@
 import { useState, useContext } from 'react';
 import { SessionContext } from '../Session';
 
-const useFetch = (endpoint, cb) => {
+const useFetch = (endpoint, cb, method = 'POST') => {
   const [loading, setLoading] = useState(false);
   const [denied, setDenied] = useState(false);
   const { loginState, setLoginState, openSnack } = useContext(SessionContext);
 
-  const call = payload => {
+  const call = payload => new Promise((resolve, reject) => {
     setLoading(true);
 
-    fetch(`${process.env.API_URL}api/${endpoint}`, {
-      method: 'POST',
+    const request = {
+      method,
       credentials: 'same-origin',
       headers: {
         Accept: 'application/json',
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify(payload),
-    })
+    };
+    let url = `${process.env.API_URL}api/${endpoint}`;
+
+    if (method === 'POST' && payload) request.body = JSON.stringify(payload);
+    if (method === 'GET' && payload) {
+      url += '?';
+      url += Object.entries(payload).map(([key, val]) => `${key}=${val}`).join('&');
+    }
+
+    fetch(url, request)
       .then(response => {
         if (response.status === 401) {
           setLoginState({ ...loginState, isLoggedIn: false });
         }
+        if (response.status === 404) {
+          openSnack('API not found', 'error');
+        }
+        if (response.status === 500) {
+          openSnack('Internal Server Error', 'error');
+        }
         response.json().then(({ data, messages, success }) => {
-          const msg = messages.length ? messages[0] : 'An unknown error occurred';
+          const errorMsg = messages.length ? messages[0] : 'An unknown error occurred';
+          const successMsg = messages.length ? messages[0] : 'Success';
 
           setDenied(!success);
           if (success && cb) cb(data);
           if (success && messages.length) openSnack(messages[0], 'success');
-          if (!success) openSnack(msg, 'error');
+          if (!success) openSnack(errorMsg, 'error');
           setLoading(false);
+
+          if (success) { resolve(successMsg); return; }
+          reject(new Error(errorMsg));
         });
-      });
-  };
+      })
+      .catch(err => console.error(err.message));
+  });
 
   return [call, loading, denied];
 };
