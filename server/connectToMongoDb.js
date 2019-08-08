eeb9a8a6b976d13df56698e816f75af88b1f35da
Michael L. Cox <m1cox@sflgnvana03.thig.com>
2019-08-08 15:36:49 -0400

    Refactored and renamed

---
+++ server/connectToMongoDb.js
@@ -0,0 +1,25 @@
+const { MongoClient } = require('mongodb');
+
+module.exports = () => {
+  const connectToMongoDB = app => new Promise(resolve => {
+    MongoClient.connect(
+      `mongodb://${process.env.DB_USER
+      }:${process.env.DB_PASSWORD
+      }@${process.env.DB_HOST
+      }/admin`, {
+        useNewUrlParser: true,
+      },
+      (err, database) => {
+        if (err) throw err;
+
+        // eslint-disable-next-line no-param-reassign
+        app.dbo = database.db(process.env.DB_NAME);
+
+        console.log('Connected to MongoDB');
+        resolve();
+      },
+    );
+  });
+
+  return connectToMongoDB;
+};
