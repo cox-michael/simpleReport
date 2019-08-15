7d4357d95fc3f039b006ff6d502c615495d3ba16
Michael L. Cox <m1cox@sflgnvana03.thig.com>
2019-08-15 09:30:03 -0400

    Reworked to be used with sockets and rest api

--- server/notPermitted.js
+++ server/notPermitted.js
@@ -1,28 +1,22 @@
-module.exports = () => (req, res, next) => {
-  res.notPermitted = options => {
-    const defaults = {
-      friendlyRejection: options.permission.toLowerCase(),
-      sendResponse: true,
-    };
-
-    const {
-      scope, permission, friendlyRejection, sendResponse,
-    } = { ...defaults, ...options };
-
-    const scopePerms = req.session.permissions[scope];
-
-    if (!scopePerms || !scopePerms.includes(permission)) {
-      console.log('Not permitted');
-      if (sendResponse) {
-        res.status(403)
-          .success(false)
-          .messages([`You do not have permissions to ${friendlyRejection}`])
-          .apiRes();
-      }
-      return true;
-    }
-    return false;
+module.exports = options => {
+  const defaults = {
+    friendlyRejection: (options && options.permission) ?
+      options.permission.toLowerCase() :
+      'do this',
+    sendResponse: true,
   };
 
-  next();
+  const {
+    scope, permission, friendlyRejection, sendResponse, cb, session,
+  } = { ...defaults, ...options };
+
+  const scopePerms = session.permissions[scope];
+
+  if (!scopePerms || !scopePerms.includes(permission)) {
+    console.log('Not permitted');
+    // console.log({ scopePerms, session, n: session.ldap.displayName });
+    if (sendResponse && cb) cb([`You do not have permissions to ${friendlyRejection}`]);
+    return true;
+  }
+  return false;
 };
