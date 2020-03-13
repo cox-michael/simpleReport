9a23666be30414dd8a7d1c2f8222792c6481b99d
Michael L. Cox <m1cox@sflgnvana03.thig.com>
2020-03-13 15:10:29 -0400

    Added Column to components index

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,5 @@
 export { default as Row } from './Row';
+export { default as Column } from './Column';
 export { default as ToggleButton } from './ToggleButton';
 export { default as ToggleButtonGroup } from './ToggleButtonGroup';
 export { default as Spinner } from './Spinner';
